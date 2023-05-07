ca13672453096d86cb9e6e3cef76183c1c8a011b
ReyRom <s03roman@mail.ru>
2023-05-07 15:18:03 +0300

    Добавьте файлы проекта.

---
+++ LinesCounter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Metrics;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Укажите каталог проекта");
+var path = Console.ReadLine();
+
+DirectoryInfo directory = new DirectoryInfo(path);
+
+var files = directory.GetFiles("",SearchOption.AllDirectories);
+int counter = 0;
+long symbols = 0;
+foreach (var file in files)
+{
+	if ((file.Extension == ".cs" || file.Extension == ".xaml") && !file.Name.Contains("AssemblyInfo") && !file.FullName.Contains("\\obj\\") && !file.FullName.Contains("\\bin\\")) //&& !Regex.IsMatch(file.Name,"[.].*[.]")
+	{
+		var data = File.ReadAllLines(file.FullName);
+		var count = data.Count(x => !string.IsNullOrWhiteSpace(x));
+		var symb = data.Sum(x => x.Length);
+		Console.WriteLine(file.Name + ": " + count + ": " + symb);
+		counter += count;
+		symbols += symb;
+	}
+}
+Console.WriteLine(counter);
+Console.WriteLine(symbols);
+Console.Read();
